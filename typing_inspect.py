fd81278cc440b6003f8298bcb22d5bc0f82ee3cd
mattHawthorn <mattHawthorn@users.noreply.github.com>
2019-04-02 13:23:47 -0400

    made _eval_args recurse in the instance that any of the args is a Callable, and added test for this case (#32)

--- typing_inspect.py
+++ typing_inspect.py
@@ -286,12 +286,13 @@ def _eval_args(args):
         if not isinstance(arg, tuple):
             res.append(arg)
         elif is_callable_type(arg[0]):
+            callable_args = _eval_args(arg[1:])
             if len(arg) == 2:
-                res.append(Callable[[], arg[1]])
+                res.append(Callable[[], callable_args[0]])
             elif arg[1] is Ellipsis:
-                res.append(Callable[..., arg[2]])
+                res.append(Callable[..., callable_args[1]])
             else:
-                res.append(Callable[list(arg[1:-1]), arg[-1]])
+                res.append(Callable[list(callable_args[:-1]), callable_args[-1]])
         else:
             res.append(type(arg[0]).__getitem__(arg[0], _eval_args(arg[1:])))
     return tuple(res)
