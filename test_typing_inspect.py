74c694fbbf3ecbcc0012b876fb449bd0afc439f8
Tomasz Trebski <tomasz.trebski@ts.fujitsu.com>
2020-07-07 10:38:00 +0200

    fixup! Respect TypedDict from typing_extensions

--- test_typing_inspect.py
+++ test_typing_inspect.py
@@ -11,25 +11,29 @@ from typing import (
 )
 
 import sys
-from mypy_extensions import TypedDict
+from mypy_extensions import TypedDict as METypedDict
+from typing_extensions import TypedDict as TETypedDict
 from typing_extensions import Literal
 
 NEW_TYPING = sys.version_info[:3] >= (3, 7, 0)  # PEP 560
 
-PY36_TESTS = """
-class TD(TypedDict):
-    x: int
-    y: int
-class Other(dict):
-    x: int
-    y: int
-"""
-
 PY36 = sys.version_info[:3] >= (3, 6, 0)
 if PY36:
-    exec(PY36_TESTS)
+
+    class TDM(METypedDict):
+        x: int
+        y: int
+
+    class TDE(TETypedDict):
+        x: int
+        y: int
+
+    class Other(dict):
+        x: int
+        y: int
+
 else:
-    TD = Other = object  # for linters
+    TDM = TDE = Other = object  # for linters
 
 
 class IsUtilityTestCase(TestCase):
@@ -76,16 +80,16 @@ class IsUtilityTestCase(TestCase):
     def test_optional_type(self):
         T = TypeVar('T')
         samples = [type(None),                # none type
-                   Optional[int],             # direct union to none type 1
-                   Optional[T],               # direct union to none type 2
-                   Optional[T][int],          # direct union to none type 3
-                   Union[int, type(None)],    # direct union to none type 4
-                   Union[str, T][type(None)]  # direct union to none type 5
-                   ]
+            Optional[int],  # direct union to none type 1
+            Optional[T],  # direct union to none type 2
+            Optional[T][int],  # direct union to none type 3
+            Union[int, type(None)],  # direct union to none type 4
+            Union[str, T][type(None)]  # direct union to none type 5
+        ]
         # nested unions are supported
         samples += [Union[str, Optional[int]],      # nested Union 1
-                    Union[T, str][Optional[int]],   # nested Union 2
-                    ]
+            Union[T, str][Optional[int]],  # nested Union 2
+        ]
         nonsamples = [int, Union[int, int], [], Iterable[Any], T, Union[T, str][int]]
         # unfortunately current definition sets these ones as non samples too
         S1 = TypeVar('S1', bound=Optional[int])
@@ -94,7 +98,7 @@ class IsUtilityTestCase(TestCase):
         S4 = TypeVar('S4', bound=Union[str, Optional[int]])
         nonsamples += [S1, S2, S3,                     # typevar bound or constrained to optional
                        Union[S1, int], S4              # combinations of the above
-                       ]
+        ]
         self.sample_test(is_optional_type, samples, nonsamples)
 
     def test_literal_type(self):
@@ -154,7 +158,7 @@ class GetUtilityTestCase(TestCase):
         self.assertEqual(get_parameters(Generic), ())
         self.assertEqual(get_parameters(Union), ())
         self.assertEqual(get_parameters(List[int]), ())
-        self.assertEqual(get_parameters(Generic[T]), (T,))
+        self.assertEqual(get_parameters(Generic[T]), (T, ))
         self.assertEqual(get_parameters(Tuple[List[T], List[S_co]]), (T, S_co))
         self.assertEqual(get_parameters(Union[S_co, Tuple[T, T]][int, U]), (U,))
         self.assertEqual(get_parameters(Mapping[T, Tuple[S_co, T]]), (T, S_co))
@@ -165,11 +169,11 @@ class GetUtilityTestCase(TestCase):
         S = TypeVar('S')
         self.assertEqual(get_last_args(int), ())
         self.assertEqual(get_last_args(Union), ())
-        self.assertEqual(get_last_args(ClassVar[int]), (int,))
+        self.assertEqual(get_last_args(ClassVar[int]), (int, ))
         self.assertEqual(get_last_args(Union[T, int]), (T, int))
         self.assertEqual(get_last_args(Iterable[Tuple[T, S]][int, T]), (int, T))
         self.assertEqual(get_last_args(Callable[[T, S], int]), (T, S, int))
-        self.assertEqual(get_last_args(Callable[[], int]), (int,))
+        self.assertEqual(get_last_args(Callable[[], int]), (int, ))
 
     @skipIf(NEW_TYPING, "Not supported in Python 3.7")
     def test_args(self):
@@ -192,7 +196,7 @@ class GetUtilityTestCase(TestCase):
 
         # ClassVar special-casing
         self.assertEqual(get_args(ClassVar, evaluate=True), ())
-        self.assertEqual(get_args(ClassVar[int], evaluate=True), (int,))
+        self.assertEqual(get_args(ClassVar[int], evaluate=True), (int, ))
 
         # Literal special-casing
         self.assertEqual(get_args(Literal, evaluate=True), ())
@@ -226,13 +230,22 @@ class GetUtilityTestCase(TestCase):
         self.assertEqual(get_generic_bases(int), ())
 
     @skipUnless(PY36, "Python 3.6 required")
-    def test_typed_dict(self):
-        TDOld = TypedDict("TDOld", {'x': int, 'y': int})
-        self.assertEqual(typed_dict_keys(TD), {'x': int, 'y': int})
+    def test_typed_dict_mypy_extension(self):
+        TDOld = METypedDict("TDOld", {'x': int, 'y': int})
+        self.assertEqual(typed_dict_keys(TDM), {'x': int, 'y': int})
         self.assertEqual(typed_dict_keys(TDOld), {'x': int, 'y': int})
         self.assertIs(typed_dict_keys(dict), None)
         self.assertIs(typed_dict_keys(Other), None)
-        self.assertIsNot(typed_dict_keys(TD), TD.__annotations__)
+        self.assertIsNot(typed_dict_keys(TDM), TDM.__annotations__)
+
+    @skipUnless(PY36, "Python 3.6 required")
+    def test_typed_dict_typing_extension(self):
+        TDOld = TETypedDict("TDOld", {'x': int, 'y': int})
+        Mself.assertEqual(typed_dict_keys(TDE), {'x': int, 'y': int})
+        self.assertEqual(typed_dict_keys(TDOld), {'x': int, 'y': int})
+        self.assertIs(typed_dict_keys(dict), None)
+        self.assertIs(typed_dict_keys(Other), None)
+        self.assertIsNot(typed_dict_keys(TDM), TDE.__annotations__)
 
 
 if __name__ == '__main__':
