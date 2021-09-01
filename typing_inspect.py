13607e3dd0c932005a2b0398b03ba57c319d8d6f
Fabian Raab <fabian@raab.link>
2021-09-01 16:46:04 +0200

    When running python2 do not check for __qualname__

--- typing_inspect.py
+++ typing_inspect.py
@@ -266,10 +266,15 @@ def is_new_type(tp):
         return False
     elif sys.version_info[:3] >= (3, 10, 0) and sys.version_info.releaselevel != 'beta':
         return tp is NewType or isinstance(tp, NewType)
-    else:
+    elif sys.version_info[:3] >= (3, 0, 0):
         return (tp is NewType or
                 (getattr(tp, '__supertype__', None) is not None and
                  getattr(tp, '__qualname__', '') == 'NewType.<locals>.new_type' and
+                 tp.__module__ == 'typing'))
+    else:  # python 2
+        # __qualname__ is not available in python 2, so we simplify the test here
+        return (tp is NewType or
+                (getattr(tp, '__supertype__', None) is not None and
                  tp.__module__ == 'typing'))
 
 
