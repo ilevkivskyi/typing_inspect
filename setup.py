82687ff741a5843eda29a4284782b0ba6c7ec65b
Ivan Levkivskyi <levkivskyi@gmail.com>
2022-08-17 14:57:27 +0100

    Bump version (#90)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 import sys
 from setuptools import setup
 
-version = '0.7.1'
+version = '0.8.0'
 description = 'Runtime inspection utilities for typing module.'
 long_description = '''
 Typing Inspect
