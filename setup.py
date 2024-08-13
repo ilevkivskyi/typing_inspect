fb640c7134facf22c118597af7a165ae8e27144a
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-08-14 00:13:00 +0300

    Add support for Python 3.12 and 3.13 (#105)
    * pytest: drop the dot
    
    * Add support for Python 3.12
    
    * Add support for Python 3.13
    
    ---------
    
    Co-authored-by: Hugo van Kemenade <hugovk@users.noreply.github.com>
    Co-authored-by: Ivan Levkivskyi <levkivskyi@gmail.com>

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ classifiers = [
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
     'Topic :: Software Development',
 ]
 
